30dfd3b6c9606ac56a777de67c7bbab37fdccb6f
edubcn75 <eduardobcn75@hotmail.com>
2026-01-02 22:04:48 +0100

    fix: update vite config and sync with remote

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+<<<<<<< Updated upstream
   base: '/edupetitvalley/', // Important!
+=======
+  // base: '/your-repo-name/', // Important!
+>>>>>>> Stashed changes
   plugins: [react()],
 })
