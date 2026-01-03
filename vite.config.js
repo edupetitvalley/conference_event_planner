1751bfcc2a1fd724c2dcb4db9328bdf7edc87620
edupetitvalley <edupetitvalley@users.noreply.github.com>
2026-01-03 04:20:11 +0100

    Update base path in Vite configuration
    Restore base path for conference event planner.

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-<<<<<<< Updated upstream
   base: '/conference_event_planner/', // Important!
-=======
   // base: '/your-repo-name/', // Important!
->>>>>>> Stashed changes
   plugins: [react()],
 })
