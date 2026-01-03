30c28c4d7ae57f75cc2ad8a65f9304b6a5e7dedb
edupetitvalley <edupetitvalley@users.noreply.github.com>
2026-01-03 04:16:38 +0100

    Change base path for Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
 <<<<<<< Updated upstream
-  base: '/edupetitvalley/', // Important!
+  base: '/conference_event_planner/', // Important!
 =======
   // base: '/your-repo-name/', // Important!
 >>>>>>> Stashed changes
